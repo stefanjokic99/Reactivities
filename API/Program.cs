26e4bc500e3855d4e886e8e7400d40912ef1bcda
stefanjokic99 <stefan.jokic.2002@student.etf.ues.rs.ba>
2022-10-02 15:57:06 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -22,6 +22,13 @@ internal class Program
             options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
 
+        builder.Services.AddCors(opt => 
+        {
+            opt.AddPolicy("CorsPolicy", policy => {
+                policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+            });
+        });
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -32,6 +39,8 @@ internal class Program
         }
 
         app.UseHttpsRedirection();
+
+        app.UseCors("CorsPolicy");
 
         app.UseAuthorization();
 
---
+++ client-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+import {Header} from 'semantic-ui-react';
+import {List} from 'semantic-ui-react';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:7198/api/activities').then(response => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+        <List>
+        {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
