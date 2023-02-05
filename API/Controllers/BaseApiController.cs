74df03aafcfd3a10433bfa1f63ab473681fd3cbb
stefanjokic99 <105276465+stefansingularity@users.noreply.github.com>
2023-02-05 17:23:58 +0100

    End of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
+using MediatR;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BaseApiController : ControllerBase
-    {
-        
-    }
+  [ApiController]
+  [Route("api/[controller]")]
+  public class BaseApiController : ControllerBase
+  {
+    private IMediator _mediator;
+
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+  }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using API.Extensions;
+
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,19 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
-});
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
